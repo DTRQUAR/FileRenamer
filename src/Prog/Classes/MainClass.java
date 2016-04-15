cff4f00639f30b1db1e52fc11cfbb58918f12112
Your Name <you@example.com>
2016-04-15 19:24:23 +0500

    Version 1.0

---
+++ src/Prog/Classes/MainClass.java
@@ -0,0 +1,12 @@
+package Prog.Classes;
+
+/**
+ * Created by Qouer on 15.04.2016.
+ */
+public class MainClass {
+    public static MainForm mainForm;
+
+    public static void main(String[] args) {
+        mainForm = new MainForm();
+    }
+}
